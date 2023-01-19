ee86a8a54159191b1f1cb0048f1e6c60827468dc
Andressa Da Costa <andressadacostan@gmail.com>
2023-01-18 22:48:17 -0300

    first-route/prisma-config

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,31 @@
 // TypeScript -> JavaScript
 
 // Typerscript -> Tipagem estática VsCode
+
+import Fastify from 'fastify';
+import { PrismaClient } from '@prisma/client';
+
+const app = Fastify();
+const prisma = new PrismaClient()
+
+/**
+ * Método HTTP: GET, POST, PUT, PATCH and DELETE
+ */
+
+app.get('/', async () => {
+	const habits = await prisma.habit.findMany({
+		where: {
+		title: {
+			startsWith: 'Beber',
+		}
+		}
+	})
+	return habits
+});
+
+app.listen({
+	port: 3333
+}).then(() => {
+	console.log('HTTP server running!');
+});
+
