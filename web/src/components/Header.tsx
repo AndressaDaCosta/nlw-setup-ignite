57ab1102f82bd86c4549e62fac487a73feb17518
Andressa Da Costa <andressadacostan@gmail.com>
2023-01-31 10:41:43 -0300

    modal-useState

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,8 +1,15 @@
 import { Plus } from 'phosphor-react';
+import * as Dialog from '@radix-ui/react-dialog';
 
 import LogoImage from '../assets/logo.svg';
+import { useState } from 'react';
 
 export function Header() {
+	const [isModalOpen, SetIsModalOpen] = useState(false);
+
+	function buttonClicked() {
+		SetIsModalOpen(true);
+	}
 	return (
 		<div className="w-full max-w-3xl mx-auto flex items-center justify-between">
 			<img
@@ -10,8 +17,17 @@ export function Header() {
 				alt=""
 			/>
 
+			{isModalOpen ? (
+				<div>
+					<div>
+						<div>modal</div>
+					</div>
+				</div>
+			) : null}
+
 			<button
 				type="button"
+				onClick={buttonClicked}
 				className="border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300">
 				<Plus
 					size={20}
