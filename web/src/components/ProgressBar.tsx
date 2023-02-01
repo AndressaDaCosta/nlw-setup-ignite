5724b5ddbd53b3f52d2636d53a1472c209690e46
Andressa Da Costa <andressadacostan@gmail.com>
2023-01-31 21:50:34 -0300

    progressBar-radix

--- web/src/components/ProgressBar.tsx
+++ web/src/components/ProgressBar.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
 import * as Progress from '@radix-ui/react-progress';
 
-// interface ProgressBarProps {
-// 	progress: number;
-// }
+interface ProgressBarProps {
+	progress: number;
+}
 
-export function ProgressBar() {
-	const [progress, setProgress] = React.useState(75);
-
-	React.useEffect(() => {
-		const timer = setTimeout(() => setProgress(75), 200);
-		return () => clearTimeout(timer);
-	}, []);
-
+export function ProgressBar(props: ProgressBarProps) {
 	return (
 		<Progress.Root
 			className="h-3 rounded-xl bg-zinc-700 w-full mt-4"
 			value={75}>
 			<Progress.Indicator
-				className="h-3 rounded-xl bg-violet-600"
-				style={{ transform: `translateX(-${100 - progress}%)` }}
+				className="h-3 rounded-xl bg-violet-600 -translate-x[-${100 - props.progress}%] transition-duration: 150ms;"
+				style={{
+					width: `${props.progress}%`
+				}}
 			/>
 		</Progress.Root>
 	);
