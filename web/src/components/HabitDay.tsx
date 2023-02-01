b13d6d4c16ec956b384cbb0b17e5018e96a1fbc8
Andressa Da Costa <andressadacostan@gmail.com>
2023-01-31 22:17:40 -0300

    progress-bar-radix-tailwind

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,36 +1,57 @@
-import * as Popover from '@radix-ui/react-popover'
+import * as Popover from '@radix-ui/react-popover';
 import clsx from 'clsx';
 import { ProgressBar } from './ProgressBar';
 
 interface HabitDayProps {
-  completed: number
-  amount: number
+	completed: number;
+	amount: number;
 }
 
-export function HabitDay({completed, amount}: HabitDayProps)) {
-  const completedPercentage = Math.round((completed / amount) * 100);
+export function HabitDay({ completed, amount }: HabitDayProps) {
+	const completedPercentage = Math.round((completed / amount) * 100);
+
 	return (
 		<Popover.Root>
-		<Popover.Trigger 
-    className={clsx("w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg", {
-      'bg-zinc-900 border-zinc-800': completedPercentage === 0,
-      'bg-violet-900 border-violet-500': completedPercentage > 0 && competedPercentage < 20,
-      'bg-violet-800 border-violet-500': completedPercentage >= 20 && completedPercentage < 40,
-      'bg-violet-700 border-violet-500': completedPercentage >= 40 && completedPercentage < 60,
-      'bg-violet-600 border-violet-500': completedPercentage >= 60 && completedPercentage < 80,
-      'bg-violet-500 border-violet-400': completedPercentage >= 80,
-    })}
-  />
-		<Popover.Portal>
-        <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
-          <span className="font-semibold text-zinc-400">terça-feira</span>
-          <span className="mt-1 font-extrabold leading-tight text-3xl">17/01</span>
+			<Popover.Trigger
+				className={clsx(
+					'w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg',
+					{
+						'bg-zinc-900 border-zinc-800':
+							completedPercentage === 0,
+						'bg-violet-900 border-violet-500':
+							completedPercentage > 0 && completedPercentage < 20,
+						'bg-violet-800 border-violet-500':
+							completedPercentage >= 20 &&
+							completedPercentage < 40,
+						'bg-violet-700 border-violet-500':
+							completedPercentage >= 40 &&
+							completedPercentage < 60,
+						'bg-violet-600 border-violet-500':
+							completedPercentage >= 60 &&
+							completedPercentage < 80,
+						'bg-violet-500 border-violet-400':
+							completedPercentage >= 80
+					}
+				)}
+			/>
+			<Popover.Portal>
+				<Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
+					<span className="font-semibold text-zinc-400">
+						terça-feira
+					</span>
+					<span className="mt-1 font-extrabold leading-tight text-3xl">
+						17/01
+					</span>
 
-          <ProgressBar progress={completedPercentage}/>
+					<ProgressBar progress={completedPercentage} />
 
-          <Popover.Arrow height={8} width={16} className='fill-zinc-900' />
-        </Popover.Content>
-      </Popover.Portal>
-    </Popover.Root>
+					<Popover.Arrow
+						height={8}
+						width={16}
+						className="fill-zinc-900"
+					/>
+				</Popover.Content>
+			</Popover.Portal>
+		</Popover.Root>
 	);
 }
--- web/src/components/ProgressBar.tsx
+++ web/src/components/ProgressBar.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import * as Progress from '@radix-ui/react-progress';
 
 interface ProgressBarProps {
@@ -7,11 +6,12 @@ interface ProgressBarProps {
 
 export function ProgressBar(props: ProgressBarProps) {
 	return (
-		<Progress.Root
-			className="h-3 rounded-xl bg-zinc-700 w-full mt-4"
-			value={75}>
+		<Progress.Root className="h-3 rounded-xl bg-zinc-700 w-full mt-4">
 			<Progress.Indicator
-				className="h-3 rounded-xl bg-violet-600 -translate-x[-${100 - props.progress}%] transition-duration: 150ms;"
+				role="progressbar"
+				aria-label="Progresso de hábitos completados nesse dia"
+				aria-valuenow={props.progress}
+				className="h-3 rounded-xl bg-violet-600  transition-transform"
 				style={{
 					width: `${props.progress}%`
 				}}
