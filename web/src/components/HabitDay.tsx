5def3a856c92c7f458c07896c662b929045bbab3
Andressa Da Costa <andressadacostan@gmail.com>
2023-02-06 11:26:53 -0300

    ReactJS

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -22,7 +22,7 @@ export function HabitDay({ completed = 0, amount = 0, date }: HabitDayProps) {
 		<Popover.Root>
 			<Popover.Trigger
 				className={clsx(
-					'w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg',
+					'w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg transition-colors focus:outline-none focus:ring-2 focus:ring-violet-600 focus:ring-offset-2 focus:ring-offset-background',
 					{
 						'bg-zinc-900 border-zinc-800':
 							completedPercentage === 0,
