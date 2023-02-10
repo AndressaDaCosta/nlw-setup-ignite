b16069b08f5acb5e1f873d2a889587d27f7c7c49
Andressa Da Costa <andressadacostan@gmail.com>
2023-02-10 16:09:59 -0300

    fixing server connection error

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,25 +2,28 @@
 
 // Typerscript -> Tipagem estática VsCode
 
-import Fastify from 'fastify'
-import cors from '@fastify/cors'
-import { appRoutes } from './routes'
-
 // import { PrismaClient } from '@prisma/client';
 
-const app = Fastify()
 // const prisma = new PrismaClient();
 
-app.register(cors)
-app.register(appRoutes)
 /**
  * Método HTTP: GET, POST, PUT, PATCH and DELETE
  */
 
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import { appRoutes } from './routes'
+
+const app = Fastify()
+
+app.register(cors)
+app.register(appRoutes)
+
 app.listen({
-	port: 3333
+	port: 3333,
+	host: '0.0.0.0'
 }).then(() => {
-	console.log('HTTP server running')
+	console.log('HTTP Server running!')
 })
 
 // host: '0.0.0.0'
--- web/src/lib/axios.ts
+++ web/src/lib/axios.ts
@@ -4,4 +4,4 @@ export const api = axios.create({
 	baseURL: 'http://192.168.0.15:3333'
 })
 
-// 	baseURL: 'https://localhost:3333'
+// baseURL: 'https://localhost:3333'
