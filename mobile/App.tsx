e5cb9f895b189e81376ef207ac084eba18ce8ff9
Andressa Da Costa <andressadacostan@gmail.com>
2023-02-13 11:08:47 -0300

    expo-notification

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,5 @@
-import './src/lib/dayjs';
-import { StatusBar } from 'react-native';
+import './src/lib/dayjs'
+import { StatusBar, Button } from 'react-native'
 
 import {
 	useFonts,
@@ -7,10 +7,11 @@ import {
 	Inter_600SemiBold,
 	Inter_700Bold,
 	Inter_800ExtraBold
-} from '@expo-google-fonts/inter';
+} from '@expo-google-fonts/inter'
 
-import { Loading } from './src/components/Loading';
-import { Routes } from './src/routes';
+import * as Notifications from 'expo-notifications'
+import { Loading } from './src/components/Loading'
+import { Routes } from './src/routes'
 
 export default function App() {
 	const [fontsLoaded] = useFonts({
@@ -18,19 +19,36 @@ export default function App() {
 		Inter_600SemiBold,
 		Inter_700Bold,
 		Inter_800ExtraBold
-	});
+	})
+
+	async function scheduleNotifications() {
+		const trigger = new Date(Date.now())
+		trigger.setMinutes(trigger.getMinutes() + 1)
+
+		await Notifications.scheduleNotificationAsync({
+			content: {
+				title: 'Olá, Andressa! 😀',
+				body: 'Você praticou seus hábitos hoje?'
+			},
+			trigger
+		})
+	}
 
 	if (!fontsLoaded) {
-		return <Loading />;
+		return <Loading />
 	}
 	return (
 		<>
+			<Button
+				title="enviar"
+				onPress={scheduleNotifications}
+			/>
 			<Routes />
 			<StatusBar
 				barStyle="light-content"
 				backgroundColor="transparent"
-				translucent
+				// translucent
 			/>
 		</>
-	);
+	)
 }
