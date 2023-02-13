45c7dab15bf0944a63de142fa969d7184978ceb6
Andressa Da Costa <andressadacostan@gmail.com>
2023-02-13 09:05:14 -0300

    new-msg-aplplication

--- web/public/service-worker.js
+++ web/public/service-worker.js
@@ -1,9 +1,10 @@
 // console.log('Running')
 
 self.addEventListener('push', function (event) {
+	const body = event.data?.text() ?? ''
 	event.waitUntil(
 		self.registration.showNotification('Habits', {
-			body: 'Testando...'
+			body
 		})
 	)
 })
