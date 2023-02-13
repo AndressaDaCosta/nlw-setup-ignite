604ec2cec1ad1da7f062233b3db1e3171eb5e600
Andressa Da Costa <andressadacostan@gmail.com>
2023-02-13 09:02:57 -0300

    service-worker

---
+++ web/public/service-worker.js
@@ -0,0 +1,9 @@
+// console.log('Running')
+
+self.addEventListener('push', function (event) {
+	event.waitUntil(
+		self.registration.showNotification('Habits', {
+			body: 'Testando...'
+		})
+	)
+})
